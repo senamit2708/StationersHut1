19bf0a858fbe42e483698dc47e30f5f5edd838bf
senamit2708 <senamit2708@gmail.com>
2017-11-11 16:46:09 +0530

    Adding constant in StationaryContract for creating uri in StationaryContentProvider

--- app/src/main/java/com/example/senamit/stationershut1/data/StationaryContract.java
+++ app/src/main/java/com/example/senamit/stationershut1/data/StationaryContract.java
@@ -1,5 +1,6 @@
 package com.example.senamit.stationershut1.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,10 +9,19 @@ import android.provider.BaseColumns;
 
 public class StationaryContract {
 
+
+    public static final String CONTENT_AUTHORITY = "com.example.senamit.stationershut1.data.StationaryContentProvider";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+CONTENT_AUTHORITY);
+
+    public static final String PATH_PRODUCTdESCRIPTIONENTRY = "ProductDescription";
+
     private StationaryContract() {
     }
 
-    public class ProductDesriptionEntry implements BaseColumns {
+    public static final class ProductDesriptionEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTdESCRIPTIONENTRY);
 
         public static final String TABLE_NAME = "ProductDescription";
         public static final String COLUMN_PRODUCT_NAME = "ProductName";
